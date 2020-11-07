6fd0e68fd6ebd4f3711554999f5004cc85230519
Julia-jh <54700208+Julia-jh@users.noreply.github.com>
2020-11-08 03:59:32 +0900

    1st project_20201114_1

---
+++ R_1st-script.R
@@ -0,0 +1,13 @@
+# 1st project
+# chapter 1) Exploratory data analysis, EDA
+# 1.1 Kinds of data
+# continuous: time
+# discrete: times
+# categorical: list
+# binary: T/F
+# ordinal: grade
+# 1.2 Rectangular data
+# data frame: table
+# feature: column, income, predictor, variables
+# outcome: dependence variables, goals
+# record: row, case, pattern, sample
