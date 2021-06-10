74511a64a7422905a01fd38c842beb20c191f688
Julia-jh <juliajhkim319@gmail.com>
2021-06-10 23:13:36 +0900

    20210610 23:12

--- R_1st-script.R
+++ R_1st-script.R
@@ -41,7 +41,7 @@ weighted.mean(state[["Murder.Rate"]],
 # install.packages("matrixStats")
 library("matrixStats")
 weightedMedian(state[["Murder.Rate"]],
-              w = state[["Population"]])
+               w = state[["Population"]])
 # 
 # 1.4 variability estimation
 # deviation: error
@@ -128,26 +128,65 @@ plot(telecom$T, telecom$VZ, xlab = "T", ylab = "VZ")
 # 
 # 1.8 explore more than two variables
 # 
-# contingency table: 
-# hexagonal binning: 
-# contour plot: 
-# violin plot: 
+# contingency table: frequency of more than two categorical variables
+# hexagonal binning
+# contour plot
+# violin plot 
 # 
-# exercise 1.8.1)
+# exercise 1.8.1-1)
 kc_tax <- read.csv(file = "data/kc_tax.csv")
 kc_tax0 <- subset(kc_tax, TaxAssessedValue < 750000 &
                     SqFtTotLiving > 100 &
                     SqFtTotLiving < 3500)
 nrow(kc_tax0)
 # 
+# exercise 1.8.1-2)
+# install.packages("ggplot2")
+# install.packages("hexbin")
+library(ggplot2)
+library(hexbin)
+ggplot(kc_tax0,
+       (aes(x = SqFtTotLiving, y = TaxAssessedValue))) +
+  stat_binhex(colour = "white") +
+  theme_bw() + 
+  scale_fill_gradient(low = "white", high = "purple") + 
+  labs(x = "Finished Square Feet", y = "Tax Assessed Value")
+# 
+# exercise 1.8.1-3)
+# install.packages("Rcpp")
+library(Rcpp)
+ggplot(kc_tax0,
+       aes(SqFtTotLiving, TaxAssessedValue)) +
+  theme_bw() + 
+  geom_point(colour = "blue", alpha = 0.1) +
+  geom_density2d(colour = "white") + 
+  labs(x = "Finished Square Feet", y = "Tax Assessed Value")
+# 
+# exercise 1.8.2
+# install.packages("descr")
+library(descr)
+lc_loans <- read.csv(file = "data/lc_loans.csv")
+x_tab <- CrossTable(lc_loans$grade, lc_loans$status,
+                      prop.c = FALSE, prop.chisq = FALSE, prop.t = FALSE)
+x_tab
+# 
+# exercise 1.8.3
+airline_stats <- read.csv(file = "data/airline_stats.csv")
+boxplot(pct_carrier_delay ~ airline, data = airline_stats, ylim = c(0, 50))
+#
+ggplot(data = airline_stats,
+       aes(airline, pct_carrier_delay))+
+       ylim(0, 50) +
+       geom_violin() + 
+       labs(x = "", y = "Daily % of Delayed Flights")
 # 
 # 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# exercise 1.8.4
+ggplot(subset(kc_tax0, ZipCode %in% c(98188, 98105, 98108, 98126)),
+       aes(x = SqFtTotLiving, y = TaxAssessedValue)) +
+       stat_binhex(colour = "white") +
+       theme_bw() +
+       scale_fill_gradient(low = "white", high = "blue") +
+       labs(x = "Finished Square Feet", y = "Tax Assessed Value") +
+       facet_wrap("ZipCode")
 # 
